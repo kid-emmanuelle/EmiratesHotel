1984af61840df9d233a2d6cdbcdffd8e25b7d224
w136761 <camille.guillier@worldline.com>
2023-12-08 10:01:14 +0100

    add hibernate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module fr.emirashotel {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    requires org.hibernate.commons.annotations;
+    requires org.hibernate.orm.core;
+
 
     opens fr.emirashotel to lombok;
     exports fr.emirashotel;
